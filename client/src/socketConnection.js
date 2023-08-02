485a0ba923bd288ffbcd71cb9c1cf842aefaf7b5
xMonUx <infinityfor0m@gmail.com>
2023-08-02 18:12:33 +0200

    Repair room entering method

--- client/src/socketConnection.js
+++ client/src/socketConnection.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import io from "socket.io-client";
 import { useNavigate } from "react-router-dom";
 
@@ -9,40 +8,36 @@ export const useSocket = () => {
     const [isConnected, setIsConnected] = useState(socket.connected);
     const navigate = useNavigate();
 
-    useEffect(() => {
-        const handleConnect = () => {
-            setIsConnected(true);
-        };
+  useEffect(() => {
+    const handleConnect = () => {
+      setIsConnected(true);
+    };
 
-        const handleRoomJoined = (roomId) => {
-            console.log(`Joined room ${roomId}`);
-            navigate(`/room/${roomId}`);
-        };
+    const handleRoomJoined = (roomId) => {
+      console.log(`Joined room ${roomId}`);
+      navigate(`/room/${roomId}`);
+    };
 
-        const handleDisconnect = () => {
-            setIsConnected(false);
-        };
+    const handleDisconnect = () => {
+      setIsConnected(false);
+    };
 
-        socket.on("connect", handleConnect);
-        socket.on("room_joined", handleRoomJoined);
-        socket.on("disconnect", handleDisconnect);
+    socket.on("connect", handleConnect);
+    socket.on("room_joined", handleRoomJoined);
+    socket.on("disconnect", handleDisconnect);
 
-        return () => {
-            socket.off("connect", handleConnect);
-            socket.off("room_joined", handleRoomJoined);
-            socket.off("disconnect", handleDisconnect);
-        };
-    }, [navigate]);
+    return () => {
+      socket.off("connect", handleConnect);
+      socket.off("room_joined", handleRoomJoined);
+      socket.off("disconnect", handleDisconnect);
+    };
+  }, [navigate]);
 
-    return { isConnected, socket };
+  return { isConnected, socket };
 };
 
-
-
 function socketConnection() {
-  return (
-    <div>socketConnection</div>
-  )
+  return null;
 }
 
-export default socketConnection
+export default socketConnection;
